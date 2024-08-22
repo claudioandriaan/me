b8650d00e468ba48d3b83d6a2d6d8f35091cfb37
claudioandriaan <claudio.andriaan@gmail.com>
2024-08-22 17:13:03 +0300

    Adding Experience section

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,37 @@ function closeMenu() {
     const menu = document.querySelector('.navbar-menu');
     menu.classList.remove('show');
 }
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         document.getElementById("navbar").style.top = "0";
+//     } else {
+//         document.getElementById("navbar").style.top = "-80px";
+//     }
+// }
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-80px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+
+// Experience
+function showDescription(element) {
+    // Hide all descriptions
+    const descriptions = document.querySelectorAll('.description');
+    descriptions.forEach(desc => desc.classList.remove('active'));
+
+    // Show the clicked description
+    const descriptionId = element.getAttribute('data-description');
+    document.getElementById(descriptionId).classList.add('active');
+
+}
